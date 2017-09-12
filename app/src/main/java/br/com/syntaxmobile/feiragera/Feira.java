cb078637e2041fa0cf749f56918f4fed7f975dc0
syntaxmobile <syntaxmobilebr@gmail.com>
2017-09-12 18:44:55 -0300

    teste

--- app/src/main/java/br/com/syntaxmobile/feiragera/Feira.java
+++ app/src/main/java/br/com/syntaxmobile/feiragera/Feira.java
@@ -1,41 +1,13 @@
 package br.com.syntaxmobile.feiragera;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class Parte extends AppCompatActivity implements View.OnClickListener {
-
-    TextView telefone, email;
+public class Feira extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parte);
-
-        telefone = (TextView) findViewById(R.id.tvTelefone);
-        telefone.setOnClickListener(this);
-        email = (TextView) findViewById(R.id.tvEmail);
-        email.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.tvTelefone:
-                String telefone = "4935211333";
-                Uri uri = Uri.parse("tel:"+telefone);
-
-                Intent it = new Intent(Intent.ACTION_DIAL);
-                startActivity(it);
-
-                break;
-
-            case R.id.tvEmail:
-                break;
-        }
+        setContentView(R.layout.feira);
     }
 }
