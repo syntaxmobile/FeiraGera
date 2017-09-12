b90d405b672efbc5367ede39200fd33361c755cd
syntaxmobile <syntaxmobilebr@gmail.com>
2017-09-12 19:23:03 -0300

    Atualização Patrocinadores

--- app/src/main/java/br/com/syntaxmobile/feiragera/Patrocinadores.java
+++ app/src/main/java/br/com/syntaxmobile/feiragera/Patrocinadores.java
@@ -47,6 +47,18 @@ public class Patrocinadores extends AppCompatActivity implements View.OnClickLis
         brde.setOnClickListener(this);
         ImageView facisc = (ImageView) findViewById(R.id.ivPat_Facisc);
         facisc.setOnClickListener(this);
+        ImageView scherer = (ImageView) findViewById(R.id.ivPat_Scherer);
+        scherer.setOnClickListener(this);
+        ImageView sicoob = (ImageView) findViewById(R.id.ivPat_Sicoob);
+        sicoob.setOnClickListener(this);
+        ImageView estrelatur = (ImageView) findViewById(R.id.ivPat_Estrelatur);
+        estrelatur.setOnClickListener(this);
+        ImageView zanardo = (ImageView) findViewById(R.id.ivPat_Zanardo);
+        zanardo.setOnClickListener(this);
+        ImageView limger = (ImageView) findViewById(R.id.ivPat_Limger);
+        limger.setOnClickListener(this);
+        ImageView crasc = (ImageView) findViewById(R.id.ivPat_Crasc);
+        crasc.setOnClickListener(this);
         ImageView pref_luzerna = (ImageView) findViewById(R.id.ivPat_PrefLuzerna);
         pref_luzerna.setOnClickListener(this);
 
@@ -142,6 +154,38 @@ public class Patrocinadores extends AppCompatActivity implements View.OnClickLis
                 it = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.luzerna.sc.gov.br/"));
                 startActivity(it);
                 break;
+
+            case R.id.ivPat_Scherer:
+                it = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.scherer-sa.com.br/"));
+                startActivity(it);
+                break;
+
+            case R.id.ivPat_Sicoob:
+                it = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.sicoob.com.br/"));
+                startActivity(it);
+                break;
+
+            case R.id.ivPat_Estrelatur:
+                //it = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.luzerna.sc.gov.br/"));
+                //startActivity(it);
+                break;
+
+            case R.id.ivPat_Zanardo:
+                it = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.grupozanardo.com.br/"));
+                startActivity(it);
+                break;
+
+            case R.id.ivPat_Limger:
+                it = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.organizacoeslimger.com.br/"));
+                startActivity(it);
+                break;
+
+            case R.id.ivPat_Crasc:
+                it = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.crasc.org.br/crasc/portal/default.php"));
+                startActivity(it);
+                break;
+
+
         }
 
     }
